ab217086d49ba85344afff2b61764a6cc85cdc0c
Suleyman Melikoglu <suleyman@melikoglu.info>
2011-04-28 14:09:29 +0300

    DataField structure is changed. Non-associated arrays are not supported now.

--- tests/XGrid/DataField/DataFieldTest.php
+++ tests/XGrid/DataField/DataFieldTest.php
@@ -17,18 +17,19 @@
           $exp = new XGrid_DataField_Text();
           $exp->setKey("My Title");
           
-          $res = XGrid_DataField::create(XGrid_DataField::TEXT, "My Title");
+          $res = XGrid_DataField::create(XGrid_DataField::TEXT, "title", "My Title");
           
       }
       
       public function testCreate2() {
           
           $exp = new XGrid_DataField_Text();
-          $exp->setKey("My Title");
+          $exp->setKey("title");
+          $exp->setTitle("My Title");
           $exp->setOptions(array("o1" => "v1", "o2" => "v2"));
           $exp->addFilter(new XGrid_Filter_Uppercase());
           
-          $res = XGrid_DataField::create(XGrid_DataField::TEXT, "My Title",
+          $res = XGrid_DataField::create(XGrid_DataField::TEXT, "title", "My Title",
                   array("o1" => "v1", "o2" => "v2"), new XGrid_Filter_Uppercase());
           
           $this->assertEquals($exp, $res);
--- tests/XGrid/XGridTest.php
+++ tests/XGrid/XGridTest.php
@@ -19,7 +19,8 @@
       
       public function testAddDataField() {
           $expected = new XGrid_DataField_Text();
-          $expected->setKey("My Key");
+          $expected->setTitle("My Key");
+          $expected->setKey("myKeyIndex");
           
           $grid = new XGrid();
           $grid->addDataField("myKeyIndex", "My Key", XGrid_DataField::TEXT);
@@ -29,7 +30,8 @@
       
       public function testAddDataField2() {
           $expected = new XGrid_DataField_Text();
-          $expected->setKey("My Key");
+          $expected->setKey("myKeyIndex");
+          $expected->setTitle("My Key");
           
           $grid = new XGrid();
           $grid->addDataField("myKeyIndex", "My Key", new XGrid_DataField_Text("My Key") );
@@ -97,8 +99,8 @@
           $expected .= "<tfoot></tfoot></table>";
           
           $grid = new XGrid();
-          $grid->addDataField(1, "Name", XGrid_DataField::TEXT);
-          $grid->addDataField(2, "SurName", XGrid_DataField::TEXT);
+          $grid->addDataField("name", "Name", XGrid_DataField::TEXT);
+          $grid->addDataField("surname", "SurName", XGrid_DataField::TEXT);
           $data = array(
               array("name" => "Value 11", "surname" => "Value 12"),
               array("name" => "Value 21", "surname" => "Value 22")
@@ -110,6 +112,9 @@
           $this->assertEquals($expected, $grid->__toString());
       }
       
+      /**
+       * non-associated arrays are not supported at the moment 
+       *
       public function testDispatchNonAssociate() {
           $expected = "<table><thead><tr><th>Name</th><th>SurName</th></tr></thead>";
           $expected .= "<tbody>";
@@ -130,7 +135,7 @@
           $grid->dispatch();
           
           $this->assertEquals($expected, $grid->__toString());
-      }
+      }*/
       
       public function testPagination() {
           $expected = "<table><thead><tr><th>Name</th><th>SurName</th></tr></thead>";
@@ -141,15 +146,15 @@
           $expected .= "<tfoot></tfoot></table>";
           
           $grid = new XGrid();
-          $grid->addDataField(1, "Name", XGrid_DataField::TEXT);
-          $grid->addDataField(2, "SurName", XGrid_DataField::TEXT);
+          $grid->addDataField("name", "Name", XGrid_DataField::TEXT);
+          $grid->addDataField("surname", "SurName", XGrid_DataField::TEXT);
           $data = array(
-              array("Value 01", "Value 00"),
-              array("Value 02", "Value 00"),
-              array("Value 03", "Value 00"),
-              array("Value 04", "Value 00"),
-              array("Value 11", "Value 12"),
-              array("Value 21", "Value 22")
+              array("name" => "Value 01", "surname" => "Value 00"),
+              array("name" => "Value 02", "surname" => "Value 00"),
+              array("name" => "Value 03", "surname" => "Value 00"),
+              array("name" => "Value 04", "surname" => "Value 00"),
+              array("name" => "Value 11", "surname" => "Value 12"),
+              array("name" => "Value 21", "surname" => "Value 22")
           );
           $dataSource = new XGrid_DataSource_Array($data);
           $grid->setDataSource($dataSource);
@@ -162,8 +167,29 @@
           
           $grid->dispatch();
           
-          $this->assertEquals($expected, $grid->__toString());
+          $this->assertEquals($expected, $grid->__toString());   
+      }
+      
+      public function testDispatchCustomFields() {
+          $expected = "<table><thead><tr><th>Name</th><th>SurName</th></tr></thead>";
+          $expected .= "<tbody>";
+          $expected .= "<tr><td>Value 11</td><td>Value 12</td></tr>";
+          $expected .= "<tr><td>Value 21</td><td>Value 22</td></tr>";
+          $expected .= "</tbody>";
+          $expected .= "<tfoot></tfoot></table>";
           
+          $grid = new XGrid();
+          $grid->addDataField("name", "Name", XGrid_DataField::TEXT);
+          $grid->addDataField("surname", "SurName", XGrid_DataField::TEXT);
+          $data = array(
+              array("id" => "my id 1", "name" => "Value 11", "surname" => "Value 12"),
+              array("id" => "my id 2", "total" => "nr", "name" => "Value 21", "surname" => "Value 22")
+          );
+          $dataSource = new XGrid_DataSource_Array($data);
+          $grid->setDataSource($dataSource);
+          $grid->dispatch();
+          
+          $this->assertEquals($expected, $grid->__toString());
       }
       
   }
