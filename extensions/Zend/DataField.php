8d034958bb8eedcb5da1ce55fa84caf0b2f9ca31
Suleyman Melikoglu <suleyman@melikoglu.info>
2011-06-02 04:38:34 -0700

    The extension class names are refactored.

--- extensions/Zend/DataField.php
+++ extensions/Zend/DataField.php
@@ -5,7 +5,7 @@
    */
   class XGrid_Zend_DataField {
       
-      const DATE = 'OS_XGrid_Zend_Datafield_Date';
-      const BUTTON = 'OS_XGrid_Zend_DataField_Buttons';
+      const DATE = 'XGrid_Zend_Datafield_Date';
+      const BUTTON = 'XGrid_Zend_DataField_Buttons';
       
   }
