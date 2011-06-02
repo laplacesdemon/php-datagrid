82f37c7f3f90c2f54d029b39b5066ba78a988020
Suleyman Melikoglu <suleyman@melikoglu.info>
2011-06-02 12:34:33 +0300

    Extensions folder is added to the root directory

---
+++ extensions/Zend/DataField.php
@@ -0,0 +1,12 @@
+<?php
+  /**
+   * The constants and definiton collection for Zend Data Fields.
+   *
+   * @author suleymanmelikoglu [at] oyunstudyosu.com
+   */
+  class OS_XGrid_Zend_DataField {
+      
+      const DATE = 'OS_XGrid_Zend_Datafield_Date';
+      const BUTTON = 'OS_XGrid_Zend_DataField_Buttons';
+      
+  }
---
+++ extensions/Zend/DataField/Date.php
@@ -0,0 +1,34 @@
+<?php
+
+  /*
+   * 
+   */
+
+  /**
+   * Description of Date
+   *
+   * @author suleyman [at] melikoglu.info
+   */
+  class OS_XGrid_Zend_Datafield_Date extends XGrid_DataField_Abstract {
+   
+      private $_format = "dd/MM/yyyy hh:mm";
+      
+      public function __construct($key = null) {
+          if(!is_null($key)) $this->setKey ($key);
+      }
+
+      public function setFormat($_format) {
+          $this->_format = $_format;
+      }
+      
+      public function getValue($object) {
+          $value = parent::getValue($object);
+          try {
+              $date = new Zend_Date($value);
+              return $date->get($this->_format);
+          } catch (Exception $e) {
+              return '';
+          }
+      }
+  
+  }
